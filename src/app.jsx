4aabd81713dba63a45dfa842aacbf77da3aafb0a
evuz <jesus_jgb_45@hotmail.com>
2019-03-02 08:55:10 +0100

    feat: remove firebase for now

--- src/app.jsx
+++ src/app.jsx
@@ -1,53 +1,18 @@
-import React, { Component } from 'react';
-import firebase from 'firebase/app';
+import React from 'react';
 
 import Header from './components/Header/Header';
-import Button from './components/Button/Button';
 
 import './app.scss';
 
-class App extends Component {
-  state = { user: null, loading: false };
-
-  githubSignIn = () => {
-    const provider = new firebase.auth.GithubAuthProvider();
-    provider.addScope('repo');
-    this.setState({ loading: true });
-    firebase
-      .auth()
-      .signInWithPopup(provider)
-      .then(({ user }) => {
-        this.setState({ user, loading: false });
-      })
-      .catch(() => this.setState({ loading: false }));
-  };
-
-  renderMain = () => {
-    const { user, loading } = this.state;
-    if (!user) {
-      return (
-        <Button
-          onClick={this.githubSignIn}
-          loading={loading}
-          size="medium"
-          color="light"
-          stl="rounded"
-        >
-          Sign in with Github
-        </Button>
-      );
-    }
-    return <p>Hello {user.displayName}</p>;
-  };
-
-  render() {
-    return (
-      <div className="app">
-        <Header />
-        <main>{this.renderMain()}</main>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div className="app">
+      <Header />
+      <main>
+        <h1>Welcome to my Blog</h1>
+      </main>
+    </div>
+  );
+};
 
 export default App;
