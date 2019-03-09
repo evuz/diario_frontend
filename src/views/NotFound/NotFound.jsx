54e0d25268f3237dcafd923315b2933107136f37
evuz <jesus_jgb_45@hotmail.com>
2019-03-09 09:33:20 +0100

    feat: use container component inside views

--- src/views/NotFound/NotFound.jsx
+++ src/views/NotFound/NotFound.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 
+import Container from '../../components/Container';
+
 import './NotFound.scss';
 
 function NotFound() {
   return (
     <main className="page-not-found">
-      <h1 className="page-not-found__message">
-        <span className="page-not-found__code">404</span>
-        <span className="page-not-found__info">Page not found</span>
-      </h1>
+      <Container>
+        <h1 className="page-not-found__message">
+          <span className="page-not-found__code">404</span>
+          <span className="page-not-found__info">Page not found</span>
+        </h1>
+      </Container>
     </main>
   );
 }
