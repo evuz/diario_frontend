a82bbd0a0afb8e9c24f11635b802ee262b5ba42a
Jesús Gómez Bellido <jesgombel@MacBook-Pro-de-Jesus.local>
2019-03-11 23:50:08 +0100

    refactor: container style&use container in header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 
 import logo from '../../assets/react-logo.svg';
+import Container from '../Container';
 
 import './Header.scss';
 
 function Header() {
   return (
-    <header className="header">
-      <img src={logo} className="header__logo" alt="logo" />
-      <h1 className="header__text">Diario de un Front-End</h1>
-    </header>
+    <Container>
+      <header className="header">
+        <img src={logo} className="header__logo" alt="logo" />
+        <h1 className="header__text">Diario de un Front-End</h1>
+      </header>
+    </Container>
   );
 }
 
