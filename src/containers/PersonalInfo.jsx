8c4fbc33b5f10d9826572e9a12f71e235770acf9
evuz <jesus_jgb_45@hotmail.com>
2019-03-16 15:15:09 +0100

    refactor: use my personal info

--- src/containers/PersonalInfo.jsx
+++ src/containers/PersonalInfo.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import MenuInfo from '../components/MenuInfo';
 
 const socialMedia = [
-  { link: 'https://www.github.com', type: 'github' },
-  { link: 'https://www.twitter.com', type: 'twitter' },
+  { link: 'https://www.github.com/evuz', type: 'github' },
+  { link: 'https://www.twitter.com/JesusGomBell', type: 'twitter' },
   { link: 'https://www.linkedin.com', type: 'linkedin' },
 ]
 const summary =
-  "Cat ipsum dolor sit amet, trip owner up in kitchen i want food lick human with sandpaper tongue who's the baby. ";
-const name = 'John Doe';
-const photo = 'http://lorempixel.com/240/240/cats';
+  "Hago de mi trabajo un hobby, intentando aprender todo lo posible de este maravilloso mundo. Procuro que no pase un día sin aprender algo nuevo";
+const name = 'Jesús Gómez';
+const photo = 'https://www.gravatar.com/avatar/33e4476999a2d0449f9c67f0b464a538?s=400';
 
 function PersonalInfo() {
   return <MenuInfo name={name} photo={photo} summary={summary} socialMedia={socialMedia} />;
