bd405e7bc2eba88f23a88c5834a927fa421934c6
evuz <jesus_jgb_45@hotmail.com>
2019-03-22 11:13:00 +0100

    feat: (functions) add created date in article

--- functions/src/server/index.ts
+++ functions/src/server/index.ts
@@ -12,7 +12,13 @@ app.use(cors({origin: true}))
 app.post('/article', isAdmin, (req, res) => {
   const article = req.body;
   article.readTime = readingTime(article.body).minutes;
-  return admin.firestore().collection('articles').add(article).then(data => {
+  return admin.firestore().collection('articles').add({
+    body: article.body,
+    title: article.title,
+    summary: article.summary,
+    read_time: readingTime(article.body).minutes,
+    created_at: Date.now()
+  }).then(data => {
     res.status(200).send(data);
   })
 });
