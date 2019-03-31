9604ab787c7607d6a5694e318b8c781d8a8b2614
evuz <jesus_jgb_45@hotmail.com>
2019-03-31 20:08:37 +0200

    feat: use login & logout in article list

--- src/views/ArticleList/ArticleList.jsx
+++ src/views/ArticleList/ArticleList.jsx
@@ -1,14 +1,18 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useContext } from 'react';
 
 import Container from '../../components/Container';
 import useDomain from '../../hooks/useDomain';
 import PersonalInfo from '../../containers/PersonalInfo';
+import GithubLoginButton from '../../containers/GithubLoginButton';
+import LogoutButton from '../../containers/LogoutButton';
 import { ArticlePreviewList } from '../../components/ArticlePreview';
 
 import './ArticleList.scss';
+import UserContext from '../../context/user/UserContext';
 
 function ArticleList() {
   const [{ data: articles }, getArticles] = useDomain('get_articles', [], true);
+  const { user } = useContext(UserContext);
 
   useEffect(() => {
     getArticles();
@@ -18,6 +22,9 @@ function ArticleList() {
       <div className="article-list">
         <div className="article-list__nav">
           <PersonalInfo />
+          <div className="article-list__btn-login">
+            {user ? <LogoutButton /> : <GithubLoginButton />}
+          </div>
         </div>
         <main className="article-list__container">
           <ArticlePreviewList articles={articles} />
